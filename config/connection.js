70b7fe3cef7f33dea0630a080ebd43f3bb54bf69
Afi-Odelia` Scruggs <afi-odeliascruggs@Afi-Odelias-MacBook-Air.local>
2017-08-08 18:30:00 -0400

    final version

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
-
+//mysql://sy851ofcvs35hfxu:un1er40cvqkjq7un@h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/p2ttuss7mb7t7nx5
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: "h40lg7qyub2umdvb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306",
+  user: "sy851ofcvs35hfxu",
+  password: "un1er40cvqkjq7un",
+  database: "p2ttuss7mb7t7nx5"
 });
 
 connection.connect(function(err) {
